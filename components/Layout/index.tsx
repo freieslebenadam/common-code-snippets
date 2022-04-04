c5c86a5d7e33e2e590ae2cbae8951afddc9d8b4d
freieslebenadam <adam.fre@seznam.cz>
2022-04-04 05:17:59 +0200

    starter

---
+++ components/Layout/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import React from 'react'
+
+type Props = {
+  children: React.ReactChild
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className='text-neutral-50'>
+      <Head>
+        <title>Daily Tasks</title>
+      </Head>
+
+      <main className='h-screen background'>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,53 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "5rem",
+          xl: "6rem",
+          "2xl": "12rem"
+        }
+      },
+      fontFamily: {
+        "sans": ["Poppins", ...defaultTheme.fontFamily.sans],
+        "mono": ["Roboto Mono", ...defaultTheme.fontFamily.mono]
+      },
+      colors: {
+        dim: {
+          "50":   "rgba(0,0,0,0.05)",
+          "100":  "rgba(0,0,0,0.10)",
+          "200":  "rgba(0,0,0,0.20)",
+          "300":  "rgba(0,0,0,0.30)",
+          "400":  "rgba(0,0,0,0.40)",
+          "500":  "rgba(0,0,0,0.50)",
+          "600":  "rgba(0,0,0,0.60)",
+          "700":  "rgba(0,0,0,0.70)",
+          "800":  "rgba(0,0,0,0.80)",
+          "900":  "rgba(0,0,0,0.90)",
+        },
+        lighter: {
+          "50":   "rgba(255,255,255,0.05)",
+          "100":  "rgba(255,255,255,0.10)",
+          "200":  "rgba(255,255,255,0.20)",
+          "300":  "rgba(255,255,255,0.30)",
+          "400":  "rgba(255,255,255,0.40)",
+          "500":  "rgba(255,255,255,0.50)",
+          "600":  "rgba(255,255,255,0.60)",
+          "700":  "rgba(255,255,255,0.70)",
+          "800":  "rgba(255,255,255,0.80)",
+          "900":  "rgba(255,255,255,0.90)",
+        },
+      },
+    },
   },
   plugins: [],
 }
