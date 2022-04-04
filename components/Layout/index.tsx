30d2008fb60984f0b85c34a15b54f3d2d7cf3f49
freieslebenadam <adam.fre@seznam.cz>
2022-04-04 19:03:24 +0200

    some seo changes

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -10,6 +10,7 @@ const Layout = ({ children }: Props) => {
     <div className='text-neutral-50 selection:bg-dim-200'>
       <Head>
         <title>Code Snippets For Every Project</title>
+        <meta name="description" content="Different code snippets I commonly use in almost every project."/>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
