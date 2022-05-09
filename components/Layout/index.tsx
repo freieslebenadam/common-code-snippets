887a5b0fb5d2bfe0d23f1958fef374dd40c74636
freieslebenadam <imafaka@email.cz>
2022-05-09 04:24:43 +0200

    Subtitle and description update fix

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: Props) => {
     <div className='text-neutral-50 selection:bg-dim-200'>
       <Head>
         <title>Code Snippets For Every Project</title>
-        <meta name="description" content='This is a "repository" type application for various code snippets I commonly use. Easily accessible and all in one place.'/>
+        <meta name="description" content='This is a &quot;repository&quot; type application for various code snippets I commonly use. Easily accessible and all in one place.'/>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
