ede561e33dd711f99df9e3e674f5eeb3467b6985
freieslebenadam <imafaka@email.cz>
2022-05-13 06:52:31 +0200

    Unused input removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { getAllSnippets } from "../utils/contentful"
 import { ISnippet } from '../types'
 import { SnippetsList } from "../components/Snippets"
@@ -24,25 +23,12 @@ type Props = {
 
 // Index page
 const Home = ({ snippets }: Props) => {
-  const [searchInput, setSearchInput] = useState("")
-
   return (
     <section className="container pt-20 animate-fade pb-10">
 
       <div className='xs:w-[25rem] text-center mx-auto'>
         <h1 className='tracking-tighter font-semibold mb-4'>Commonly Used Code Snippets</h1>
         <p className='opacity-80'>This is a &quot;repository&quot; type application for various code snippets I commonly use. Easily accessible and all in one place.</p>
-      </div>
-
-      <div className='py-10'>
-        <input type="text" 
-          placeholder='Search...'
-          tabIndex={0}
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          onClick={() => setSearchInput("")}
-          className="px-4 py-2 rounded-md mx-auto block shadow bg-lighter-200 text-lighter-900 placeholder:text-lighter-400 focus:outline-none outline-none ring-0 focus:ring-0 transition-200 focus:shadow-xl focus:scale-[1.01] border border-lighter-100 sm:w-[30rem] w-full"
-        />
       </div>
 
       <SnippetsList snippets={snippets} />
